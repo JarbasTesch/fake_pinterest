4eaba03d862af36c8f76b2c65e72d90a92682f13
Jarbas Tesch <jaarbas200@gmail.com>
2024-11-04 17:15:54 -0300

    developing 1

--- FakePinterest/routes.py
+++ FakePinterest/routes.py
@@ -1,18 +1,25 @@
-from flask import render_template, url_for
-from FakePinterest import app
-from flask_login import login_required
-
+from flask import render_template, url_for, redirect
+from FakePinterest import app, database, bcrypt
+from flask_login import login_required, login_user, logout_user
 from FakePinterest.forms import FormLogin, FormCriarConta
-
+from FakePinterest.models import Usuario, Foto
 
 @app.route("/", methods = ["GET", "POST"])
 def homepage():
-    formlogin = FormLogin
+    formlogin = FormLogin()
     return render_template("homepage.html", form=formlogin)
 
 @app.route("/criarconta", methods = ["GET", "POST"])
 def criarconta():
-    formcriarconta = FormCriarConta
+    formcriarconta = FormCriarConta()
+    if formcriarconta.validate_on_submit():
+        senha = bcrypt.generate_password_hash(formcriarconta.senha.data)
+        usuario = Usuario(username=formcriarconta.username.data, senha=senha, email=formcriarconta.email.data)
+        database.session.add(usuario)
+        database.session.commit()
+        login_user(usuario, remember=True)
+        return redirect(url_for("perfil", usuario=usuario.username))
+
     return render_template("criarconta.html", form=formcriarconta)
 
 @app.route("/perfil/<usuario>")
